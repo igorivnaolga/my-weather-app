8eae3f1b20001f5cfeb0a0bcdcc9c804600bf111
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2022-08-27 16:55:13 +1200

    added search engine

--- src/app.js
+++ src/app.js
@@ -42,8 +42,21 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "ff833e479a6ba8688180b478fbf4489b";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "ff833e479a6ba8688180b478fbf4489b";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Christchurch");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
