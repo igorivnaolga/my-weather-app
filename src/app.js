dd78d4f09c68249d7cc81115e7e3ffca3ab92357
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2022-08-30 18:06:32 +1200

    created function displayForecast

--- src/app.js
+++ src/app.js
@@ -21,6 +21,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = [" Wed", "Thu", "Fri", "Sat"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+                  <div class="weather-forecast-date">
+                  ${day}</div>
+                  <img
+                    src="http://openweathermap.org/img/wn/01d@2x.png"
+                    width="36px"
+                    alt=""
+                  />
+                  <div class="weather-forecast-temperature">
+                    <span class="weather-forecast-temperature-max">18° </span>
+                    <span class="weather-forecast-temperature-min">12° </span>
+                  </div>
+                </div>
+              `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -102,3 +130,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Christchurch");
+displayForecast();
